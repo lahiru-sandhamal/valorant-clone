8285581663f2b5bbf93d3d56094c3db854b0f36a
sandhamal <lahirusandamalrohitha@gmail.com>
2025-03-03 01:49:46 +0530

    second

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCl9zy3e0HNSZ-SdWvttokMXasi8m884ZE",
+    authDomain: "valorant---clone.firebaseapp.com",
+    projectId: "valorant---clone",
+    storageBucket: "valorant---clone.firebasestorage.app",
+    messagingSenderId: "171476930589",
+    appId: "1:171476930589:web:e16fe62923f891b800b6f4"
+};
+
+// Initialize Firebase
+const app = initializeApgit add .
+    git commit -m "Your commit message"
+git push origin main
+p(firebaseConfig);
